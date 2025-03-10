f1d1df3d1cf6957fc9477e1ceb6937efbd3ec7e2
tyVespA <migliavacca.marco08@gmail.com>
2025-03-10 19:26:15 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/data").then((res) => {
+      console.log("promise fullfilled");
+      setData(res.data);
+    });
+  }, []);
+  console.log("render", data.length, "data points");
+
+  function formatWeight(weight: number) {
+    return weight % 1 === 0 ? `${weight}.0` : `${weight}`;
+  }
+
   return (
     <>
       <h1>Weight tracker</h1>
       <img src={reactLogo} alt="" />
+      {data.map((dataPoint) => (
+        <p>{formatWeight(dataPoint.weight)} kg</p>
+      ))}
     </>
   );
 }
