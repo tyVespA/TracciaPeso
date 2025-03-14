c7cc8a360d20864eb189c1c208297c083db0a284
tyVespA <migliavacca.marco08@gmail.com>
2025-03-14 12:57:28 +0100

    Update WeightsListItem.tsx

--- src/components/WeightsListItem.tsx
+++ src/components/WeightsListItem.tsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
+import styles from "./WeightsListItem.module.css";
 import formatWeight from "../utils/formatWeight";
+import WeightsListItemModal from "./WeightsListItemModal";
 
 interface WeightsListItemProps {
-  key: string;
+  id: string;
   weight: number;
+  weights: { id: string; weight: number }[];
+  setWeights: (weights: { id: string; weight: number }[]) => void;
+  setReload: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function WeightsListItem({ key, weight }: WeightsListItemProps) {
-  return <p key={key}>{formatWeight(weight)} kg</p>;
+export default function WeightsListItem({
+  id,
+  weights,
+  weight,
+  setWeights,
+  setReload,
+}: WeightsListItemProps) {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <button
+        key={id}
+        className={styles.button}
+        onClick={() => {
+          setOpenModal(!openModal);
+          console.log(openModal);
+          console.log(id);
+        }}
+      >
+        {formatWeight(weight)} kg
+      </button>
+      {openModal ? (
+        <WeightsListItemModal
+          id={id}
+          weights={weights}
+          weight={weight}
+          setWeights={setWeights}
+          setOpenModal={setOpenModal}
+          setReload={setReload}
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
