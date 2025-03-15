f7cfe9a4caa95fab3219485ca7866ea58d443be1
tyVespA <migliavacca.marco08@gmail.com>
2025-03-15 17:56:24 +0100

    Create handleErrorMessage.ts

---
+++ src/utils/handleErrorMessage.ts
@@ -0,0 +1,32 @@
+interface HandleErrorMessageProps {
+  newWeight: string | number;
+  setErrorMessage: (message: string) => void;
+  setErrorState: (state: boolean) => void;
+}
+
+export default function handleErrorMessage({
+  newWeight,
+  setErrorMessage,
+  setErrorState,
+}: HandleErrorMessageProps) {
+  let error = "";
+
+  if (newWeight === null || newWeight === undefined || newWeight === "") {
+    error = "Type a weight before submitting";
+  } else if (isNaN(Number(newWeight))) {
+    error = "Weight can't contain letters";
+  } else if (Number(newWeight) < 0) {
+    error = "Weight can't be less than 0";
+  } else if (Number(newWeight) > 299) {
+    error = "Weight can't be more than 300";
+  }
+
+  if (error) {
+    setErrorMessage(error);
+    setErrorState(true);
+  }
+
+  setTimeout(() => {
+    setErrorState(false);
+  }, 4000);
+}
