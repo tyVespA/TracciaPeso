ce4863d717d5d6bb31e6e1cec296000ac83823cd
tyVespA <migliavacca.marco08@gmail.com>
2025-03-18 15:17:30 +0100

    modify base url to .env variable

--- src/services/weights.ts
+++ src/services/weights.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "/api/weights";
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("token");
